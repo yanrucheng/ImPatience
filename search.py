0b42e3e5e8670b6393ef52f611f81ba20f7bc0f9
Cheng Yanru <yanrcheng2@gmail.com>
2019-12-06 17:54:39 +0800

    Make move a seperate obhject. Improve searching.

--- search.py
+++ search.py
@@ -1,39 +1,90 @@
 from solitaire import Game
-import collections, argparse
+import collections, argparse, random
 import heapq
 
-def dfs(game):
+def dfs(game, args):
     dp = collections.deque([game.start_state])
     met = set()
 
     while dp:
         state = dp.pop()
-        #state.visualize()
+        if args.verbose:
+            state.visualize()
 
         if game.is_goal_state(state):
             return state.solution
 
-        for s in game.get_successors(state):
+        important_states, other_states = game.get_successors(state)
+        for s in important_states + other_states:
             if s not in met:
                 dp.append(s)
             met.add(s)
 
-def a_star(game):
-    dp = [(27,0,game.start_state)]
-    met = set()
+def a_star(game, args):
+    seed = args.seed
+    while True:
+        solution = a_star_solver(game, verbose=args.verbose, max_depth=args.maxdepth)
+        if solution: break
+        print('Fail with seed {}'.format(seed))
+        seed += 1
+        random.seed(seed)
+        break
+    return solution
 
-    def heuristic(state):
+def a_star_solver(game, verbose=False, max_depth=None):
+
+    def score(state):
         goal = 27 - sum(state.goal_buffer.values())
         return goal
 
+    def dig_out_distance(state):
+        # assume on average it tooks 100 steps to complete the game
+        # assume the last 7 steps are all score step
+        TOTAL_STEP = 100
+        LAST_N_STEP = 7
+
+        average_step = (TOTAL_STEP - LAST_N_STEP) / (27 - LAST_N_STEP)
+        complete = sum(state.goal_buffer.values())
+
+        if complete > 27 - LAST_N_STEP:
+            padding = 27 - complete
+        else:
+            padding = complete * average_step
+
+        targets = set(k+str(v+1) for k,v in state.goal_buffer.items())
+        dig_out_distances = []
+        dig_out_distances += [len(column)-column.index(card)-1 for column in state.deck for card in column if card in targets]
+        dig_out_distances += [0 for card in state.goal_buffer if card in targets]
+        dig_out_distance = min(dig_out_distances)
+
+        return dig_out_distance + padding
+
+    heuristic = dig_out_distance
+    initial_h = 100
+    dp = [(initial_h,0,game.start_state)]
+    met = set()
+
     while dp:
         _, cost, state = heapq.heappop(dp)
-        state.visualize()
+
+        # give up
+        if max_depth and cost > max_depth: return
+
+        if verbose:
+            state.visualize()
 
         if game.is_goal_state(state):
             return state.solution
 
-        for s in game.get_successors(state):
+        important_states, other_states = game.get_successors(state)
+        if len(important_states) > 3:
+            states = important_states
+        elif len(important_states) + len(other_states) < 3:
+            states = important_states + other_states
+        else:
+            states = important_states + random.sample(other_states, 3-len(important_states))
+
+        for s in states:
             if s not in met:
                 heapq.heappush(dp, (cost+1+heuristic(s), cost+1, s))
             met.add(s)
@@ -43,17 +94,20 @@ def arg_parser_setup():
     parser = argparse.ArgumentParser(description='Test Game logic.')
     parser.add_argument('-f', '--filename', default='test/case1.txt', help='Input test case filename.')
     parser.add_argument('-m', '--method', default='dfs', help='Search method selection')
-    parser.add_argument('-v', '--verbose', default=True, help='Whether to print out solution in console')
+    parser.add_argument('-d', '--maxdepth', default=200, type=int, help='Max searching depth for a_star algorithm')
+    parser.add_argument('-s', '--seed', default=2019, type=int, help='Random seed')
+    parser.add_argument('-v', '--verbose', default=False, action='store_true', help='Whether to print out searching details')
     return parser.parse_args()
 
 def main():
-    search_func_dict = { 'dfs': dfs, 'a_star': a_star, }
+    search_func_dict = { 'dfs': dfs, 'a_star': a_star}
     args = arg_parser_setup()
-    game = Game(args.filename)
-    res = search_func_dict[args.method](game)
 
-    if args.verbose:
-        for i,x in enumerate(res): print('Step {}: {}'.format(i, x))
+    random.seed(args.seed)
+    game = Game(args.filename)
+    actions = search_func_dict[args.method](game, args)
+
+    game.print_actions(actions)
 
 
 if __name__ == '__main__':
--- solitaire.py
+++ solitaire.py
@@ -19,7 +19,7 @@ class GameState:
     There are 8 columns on the deck. each start with 5 cards.
     '''
     __slots__ = ['buffer_area', 'buffer_size', 'goal_buffer', 'collected', 'deck', 'flower_in_deck',
-            'solution', 'history', 'locked_to_locker', 'locker_to_locked']
+            'solution', 'history', 'locked_to_keys', 'key_to_locked']
 
     ORIGINAL_BUFFER_SIZE = 3
     GOALS = ['r', 'g', 'b']
@@ -36,8 +36,8 @@ class GameState:
         # solution is not calculated in hash
         self.solution = []
         self.history = set()
-        self.locked_to_locker = {}
-        self.locker_to_locked = {}
+        self.locked_to_keys = {}
+        self.key_to_locked = {}
 
     def __hash__(self):
         deck = ' '.join(sorted(''.join(l) for l in self.deck))
@@ -64,8 +64,8 @@ class GameState:
 
         state.solution = list(self.solution)
         state.history = set(self.history)
-        state.locked_to_locker = dict(self.locked_to_locker)
-        state.locker_to_locked = dict(self.locker_to_locked)
+        state.locked_to_keys = dict(self.locked_to_keys)
+        state.key_to_locked = dict(self.key_to_locked)
 
         return state
 
@@ -75,19 +75,23 @@ class GameState:
     def visualize(self):
         print()
         print('Buffer area: {}'.format(', '.join([str(c) for c in self.buffer_area])))
-        print('Goal area: {}'.format(', '.join([k+str(v) for k,v in self.goal_buffer.items() if v])))
+        print('Goal area: {}'.format(', '.join([k+str(v) for k,v in self.goal_buffer.items()])))
         print('Deck:')
         row_fmt = '{:>7}' * self.DECK_COLUMN_NUM
         for i in range(max(len(c) for c in self.deck)):
             print(row_fmt.format(*[str(c[i]) if len(c)>i else '' for c in self.deck]))
         print()
 
+MoveAction = collections.namedtuple('MoveAction', ['card', 'source', 'source_position', 'to', 'to_position'])
+ScoreAction = collections.namedtuple('ScoreAction', ['card', 'source', 'source_position'])
+CollectAction = collections.namedtuple('CollectAction', ['target'])
+
 class Game:
     '''This class control the game flow'''
     def __init__(self, deck_filename='test/case1.txt'):
-        self.start_state = self.get_start_state(deck_filename)
+        self.start_state = self._get_start_state(deck_filename)
 
-    def get_start_state(self, deck_filename):
+    def _get_start_state(self, deck_filename):
         state = GameState()
         with open(deck_filename, 'r') as f:
             lines = f.readlines()
@@ -95,14 +99,14 @@ class Game:
                 for x in line.strip().split():
                     column.append(x)
         state.record()
-        self.is_valid_state(state)
+        assert self.is_valid_state(state), 'invalid state'
         return state
 
     def is_valid_state(self, state):
         cards = sum([list(c) for c in state.deck], []) + state.buffer_area # flatten all the cards in deck
         # check buffer size
         try:
-            assert len(state.buffer_area) <= state.buffer_size - sum(state.collected.values()), \
+            assert len(state.buffer_area) <= state.buffer_size, \
                 'Buffer area overflow. Buffer: {}, collected: {}'.format(
                     state.buffer_area, [x for x,v in state.collected.items() if v])
 
@@ -115,129 +119,155 @@ class Game:
 
             # check cards with numbers
             d = collections.defaultdict(list)
-            _ = [ d[x[0]].append(x[1]) for x in state.GOALS if len(x)==2 ]
-            assert all(list(range(state.goal_buffer[x]+1,10)) == sorted(nums) for x,nums in d.items()), \
-                'Number card is missing'
+            _ = [ d[get_type(card)].append(get_num(card)) for column in state.deck for card in column if len(card)==2 ]
+            _ = [ d[get_type(card)].append(get_num(card)) for card in state.buffer_area ]
+            for x,nums in d.items():
+                missing = set(range(state.goal_buffer[x]+1,10)) - set(nums)
+                if missing:
+                    raise AssertionError('{} is missing'.format('{}{}'.format(x, list(missing)[0])))
 
         except AssertionError as e:
             state.visualize()
-            raise e
+            print(e)
+            return False
+        else:
+            return True
 
     def is_goal_state(self, state):
         # check whether is goal state
         return all(x == 9 for x in state.goal_buffer.values())
 
-    def take_action(
-        self, state,
-        source='deck', source_position='', to='goal', to_position='',
-        collect_target=None, auto_proceed=True
-    ):
+    def _lock_state(self, state, action):
+        '''Internal function for performance improvement.'''
+        if isinstance(action, ScoreAction) or isinstance(action, CollectAction):
+            return
+        # put a lock to the destination position
+        locked = (action.to, action.to_position)
+        # action involving key positions will undo the lock on a position
+        # locked positon itself is also a key position
+        keys = [(action.source, action.source_position), locked]
+        state.locked_to_keys[locked] = keys
+        for k in keys:
+            state.key_to_locked[k] = locked
+
+    def _unlock_state(self, state, action):
+        '''Internal function for performance improvement.'''
+        def unlock(key):
+            if key in state.key_to_locked:
+                locked = state.key_to_locked[key]
+                for locker in state.locked_to_keys[locked]:
+                    del state.key_to_locked[locker]
+                del state.locked_to_keys[locked]
+
+        if isinstance(action, MoveAction):
+            keys = [(action.source, action.source_position),
+                    (action.to, action.to_position)]
+        elif isinstance(action, ScoreAction):
+            keys = [(action.source, action.source_position)]
+        elif isinstance(action, CollectAction):
+            # collect flower does not unlock anything
+            if action.target == 'flower': return
+            # collect color cards does unlock 4 positions at most
+            keys = [('buffer', i) for i,card in enumerate(state.buffer_area) if card==action.target] +\
+                   [('deck', i) for i, column in enumerate(state.deck) if column and column[-1]==action.target]
+
+        for k in keys: unlock(k)
+
+    def _is_action_locked(self, state, action):
+        '''Internal function for performance improvement.'''
+        if isinstance(action, ScoreAction) or isinstance(action, CollectAction):
+            return False
+
+        return (action.source, action.source_position) in state.locked_to_keys
+
+    def take_actions(self, state, actions, visualize=False):
+        for action in actions:
+            state = self.take_action(state, action, auto_proceed=False)
+            if visualize: state.visualize()
+        return state
+
+    def take_action( self, state, action, auto_proceed=True):
         '''Generate a new successor state based on the given action. Record the new hash'''
 
-        def unlock(state, inv):
-            if inv in state.locker_to_locked:
-                locked = state.locker_to_locked[inv]
-                for locker in state.locked_to_locker[locked]:
-                    del state.locker_to_locked[locker]
-                del state.locked_to_locker[locked]
-
-        # determine whether this action is locked
-        if (source == 'deck' and source_position in state.locked_to_locker) or\
-                (source == 'buffer' and source_position + state.DECK_COLUMN_NUM in state.locked_to_locker):
-            return None
-
-        target_map = {'goal':'', 'buffer':len(state.buffer_area)+state.DECK_COLUMN_NUM, 'deck':to_position}
-        source_map = {'deck':source_position,
-            'buffer':source_position+state.DECK_COLUMN_NUM if isinstance(source_position, int) else 0}
+        if self._is_action_locked(state, action): return None
 
         new_state = state.copy()
-        if not collect_target:
-            card = new_state.deck[source_position].pop() if source == 'deck' else \
-                    new_state.buffer_area.pop(source_position)
 
-            # locking process
-            if to != 'goal':
-                locked = target_map[to]
-                involved = [locked, source_map[source]]
-                # if not put to goal, the destination is locked
-                new_state.locked_to_locker[locked] = involved
-                for l in involved:
-                    new_state.locker_to_locked[l] = locked
+        if isinstance(action, MoveAction):
 
-            # unlocking process
-            involved = [source_map[source], target_map[to]]
-            for inv in involved:
-                unlock(new_state, inv)
+            # handling card source
+            if action.source == 'deck': new_state.deck[action.source_position].pop()
+            elif action.source == 'buffer': new_state.buffer_area.pop(action.source_position)
 
             # handling card destination
-            if to=='goal':
-                new_state.goal_buffer[get_type(card)] = get_num(card)
-            elif to=='deck':
-                new_state.deck[to_position].append(card)
-            elif to=='buffer':
-                new_state.buffer_area.append(card)
+            if action.to=='deck':
+                new_state.deck[action.to_position].append(action.card)
+            elif action.to=='buffer':
+                new_state.buffer_area.append(action.card)
 
+        elif isinstance(action, ScoreAction):
+            if action.source == 'deck': new_state.deck[action.source_position].pop()
+            elif action.source == 'buffer': new_state.buffer_area.pop(action.source_position)
+            new_state.goal_buffer[get_type(action.card)] = get_num(action.card)
 
-        elif  collect_target == 'flower':
-            new_state.flower_in_deck = False
-            for i in range(len(new_state.deck)):
-                if new_state.deck[i] and new_state.deck[i][-1] == 'f':
-                    new_state.deck[i].pop(); break
+        elif isinstance(action, CollectAction):
+            if action.target == 'flower':
+                new_state.flower_in_deck = False
+                for i in range(len(new_state.deck)):
+                    if new_state.deck[i] and new_state.deck[i][-1] == 'f':
+                        new_state.deck[i].pop(); break
+            else:
+                new_state.buffer_size -= 1
+                new_state.buffer_area = [x for x in new_state.buffer_area if x != action.target]
 
-            # collect flower does not unlock anything
+                for i,c in enumerate(new_state.deck):
+                    if c and c[-1] == action.target:
+                        c.pop()
+                new_state.collected[action.target] = True
 
-        else:
-            involved = []
-            if collect_target in new_state.buffer_area: involved.append('buffer')
+        if hash(new_state) in new_state.history: return None
 
-            new_state.buffer_size -= 1
-            new_state.buffer_area = [x for x in new_state.buffer_area if x != collect_target]
-
-            for i,c in enumerate(new_state.deck):
-                if c and c[-1] == collect_target:
-                    c.pop()
-                    involved.append(i)
-            new_state.collected[collect_target] = True
-
-            for inv in involved:
-                unlock(new_state, inv)
-
-
-        if hash(new_state) in new_state.history:
-            return None
-
+        self._lock_state(new_state, action)
+        self._unlock_state(new_state, action)
         new_state.record()
-        if collect_target:
-            new_state.solution.append('collect {}'.format(collect_target))
-        else:
-            if isinstance(source_position, int): source_position += 1
-            if isinstance(to_position, int): to_position += 1
-            new_state.solution.append('{} at {} {} to {} {}'.format(card, source, source_position, to, to_position))
+        new_state.solution.append(action)
 
-        if auto_proceed:
-            new_state = self.auto_proceed(new_state)
+        if auto_proceed: new_state = self.auto_proceed(new_state)
 
         return new_state
+
+    def print_actions(self, actions):
+        def action_str(action):
+            if isinstance(action, CollectAction):
+                return 'collect {}'.format(action.target)
+            elif isinstance(action, ScoreAction):
+                return '{} at {} {} to goal'.format(*action)
+            elif isinstance(action, MoveAction):
+                return '{} at {} {} to {} {}'.format(*action)
+
+        for i,action in enumerate(actions, 1):
+            print('Step {}: {}'.format(i, action_str(action)))
 
     def auto_proceed(self, state):
         # get rid of the flower once met
         if state.flower_in_deck and 'f' in [c[-1] for c in state.deck if c]:
-            state = self.take_action(state, collect_target='flower', auto_proceed=False)
+            state = self.take_action(state, CollectAction('flower'), auto_proceed=False)
             assert state, 'auto proceed failure'
 
         # put the smallest exposed card to goal
         def auto_put_to_goal(state):
-            type, target = min(state.goal_buffer.items(), key=lambda x:x[1])
-            for i,c in enumerate(state.deck):
-                if c and get_type(c[-1]) == type and get_num(c[-1]) == target+1:
-                    state = self.take_action(state, source='deck', source_position=i, to='goal', auto_proceed=False)
+            color, min_value = min(state.goal_buffer.items(), key=lambda x:x[1])
+            target_card = color+str(min_value+1)
+            for i,column in enumerate(state.deck):
+                if column and column[-1] == target_card:
+                    state = self.take_action(state, ScoreAction(target_card, 'deck', i), auto_proceed=False)
                     assert state, 'auto proceed failure'
                     return state
 
         while True:
-            tmp = auto_put_to_goal(state)
-            if not tmp: break
-            state = tmp
+            new_state = auto_put_to_goal(state)
+            if not new_state: break
+            state = new_state
 
         return state
 
@@ -246,51 +276,44 @@ class Game:
         # add code to return list of successor states
         if self.is_goal_state(state): return []
 
-        res = [] # if the successor is already visited, a None will be stored in res
-        def collect_all_collectables(state, type):
-            if not state.collected[type] and (len(state.buffer_area) - state.buffer_size or type in state.buffer_area) and\
-                    sum(x[-1]==type for x in state.deck if x) + sum(x==type for x in state.buffer_area) == 4:
-                res.append(self.take_action(state, collect_target=type))
+        # if the successor is already visited, a None will be stored in states
+        other_state = []
+        important_states = []
 
-        for x in state.GOALS:
-            collect_all_collectables(state, x)
+        def try_collect_all_collectables(color):
+            if not state.collected[color] and (len(state.buffer_area) - state.buffer_size or color in state.buffer_area) and\
+                    sum(x[-1]==color for x in state.deck if x) + sum(x==color for x in state.buffer_area) == 4:
+                important_states.append(self.take_action(state, CollectAction(color)))
 
-        for i,c in enumerate(state.deck):
-            if not c: continue
-            card = c[-1]
+        for color in state.GOALS:
+            try_collect_all_collectables(color)
+
+        for i,column in enumerate(state.deck):
+            if not column: continue
+            card = column[-1]
 
             if get_num(card) and state.goal_buffer[get_type(card)] == get_num(card) - 1:
-                res.append(self.take_action(state, source='deck', source_position=i, to='goal'))
+                important_states.append(self.take_action(state, ScoreAction(card, 'deck', i)))
 
             for j, col in enumerate(state.deck):
-                if col and get_num(card) and j != i and \
-                        get_type(col[-1]) != get_type(card) and get_num(col[-1]) == get_num(card) + 1:
-                    res.append(self.take_action(state, source='deck', source_position=i, to='deck', to_position=j))
-                elif not col:
-                    res.append(self.take_action(state, source='deck', source_position=i, to='deck', to_position=j))
+                if (not col) or (get_num(card) and j != i and \
+                        get_type(col[-1]) != get_type(card) and get_num(col[-1]) == get_num(card) + 1):
+                    other_state.append(self.take_action(state, MoveAction(card, 'deck', i, 'deck', j)))
 
             if len(state.buffer_area) < state.buffer_size:
-                res.append(self.take_action(state, source='deck', source_position=i, to='buffer'))
+                other_state.append(self.take_action(state, MoveAction(card, 'deck', i, 'buffer', None)))
 
         for i,card in enumerate(state.buffer_area):
             if get_num(card) and state.goal_buffer[get_type(card)] == get_num(card) - 1:
-                res.append(self.take_action(state, source='buffer', source_position=i, to='goal'))
+                important_states.append(self.take_action(state, ScoreAction(card, 'buffer', i)))
 
             for j, col in enumerate(state.deck):
-                if col and get_num(card) and j != i and \
-                        get_type(col[-1]) != get_type(card) and get_num(col[-1]) == get_num(card) + 1:
-                    res.append(self.take_action(state, source='buffer', source_position=i, to='deck', to_position=j))
-                elif not col:
-                    res.append(self.take_action(state, source='buffer', source_position=i, to='deck', to_position=j))
+                if (not col) or (get_num(card) and j != i and \
+                        get_type(col[-1]) != get_type(card) and get_num(col[-1]) == get_num(card) + 1):
+                    other_state.append(self.take_action(state, MoveAction(card, 'buffer', i, 'deck', j)))
 
-        states = []
-        met = set()
-        for x in res:
-            if x and x not in met:
-                met.add(x)
-                states.append(x)
-
-        return states
+        other_state = list(set(state for state in other_state if state))
+        return important_states, other_state
 
 def arg_parser_setup():
     parser = argparse.ArgumentParser(description='Test Game logic.')
